f12ba7fc2ae47b946dc8ef869cc929c430e3e1a7
Tiêu Kiếm <107684026+realtieukiem123@users.noreply.github.com>
2024-01-05 11:17:40 +0700

    05/01

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+
+    [SerializeField] public Transform[] AllTranform;
+    [SerializeField] public Slider silerTask;
+
+
+}
--- Assets/Scripts/MoveAI.cs
+++ Assets/Scripts/MoveAI.cs
@@ -7,6 +7,12 @@ public class MoveAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent agent;
+    Animator botAnim;
+    bool isRunning = true;
+    private void Awake()
+    {
+        ChangerTarget();
+    }
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -16,7 +22,39 @@ public class MoveAI : MonoBehaviour
 
     private void Update()
     {
-        agent.SetDestination(target.position);
+        if (!isRunning) return;
+        if ((target.position.x - this.transform.position.x) < 0.1f && (target.position.y - this.transform.position.y) < 0.1f)
+        {
+            isRunning = false;
+            StartCoroutine(UpdateTask());
+        }
+        else
+        {
+            agent.SetDestination(target.position);
+        }
+
+    }
+    IEnumerator UpdateTask()
+    {
+        yield return new WaitForSeconds(5f);
+
+        ChangerTarget();
+        if (GameManager.Instance.silerTask.value < 1)
+        {
+            GameManager.Instance.silerTask.value += 0.01f;
+        }
+        else
+        {
+            //lose
+            GameManager.Instance.silerTask.value = 1f;
+        }
+
+    }
+    private void ChangerTarget()
+    {
+        isRunning = true;
+        int i = Random.Range(0, 10);
+        target = GameManager.Instance.AllTranform[i];
     }
 
 
