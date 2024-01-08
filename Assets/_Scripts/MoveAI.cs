69e583e22788c6e3b564d87972b929389147032c
Tiêu Kiếm <107684026+realtieukiem123@users.noreply.github.com>
2024-01-08 11:50:11 +0700

    08/01

--- Assets/_Scripts/MoveAI.cs
+++ Assets/_Scripts/MoveAI.cs
@@ -1,25 +1,70 @@
+using Spine;
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class MoveAI : MonoBehaviour
 {
+    public ColorBot colorBot;
     Transform target;
     NavMeshAgent agent;
-    Animator botAnim;
-    SpriteRenderer botSprite;
+    SkeletonAnimation anim;
     bool isRunning = true;
-
+    private string[] aiNames = {
+"Player1", "GamerX", "ProGamer123", "GameMaster", "EpicPlayer",
+    "SpeedyGamer", "NoobSlayer", "PixelWarrior", "StarGamer", "VictoryChaser",
+    "GameAddict", "TheGamingNinja", "PlayfulPanda", "DragonGamer", "OnlineWarrior",
+    "GameFreak", "CyberHero", "ControllerKing", "BattleLegend", "QuestHunter",
+    "GameWizard", "VirtualHero", "GamingGuru", "UltraGamer", "JoystickJunkie",
+    "ActionHero", "GameOverlord", "StealthGamer", "ConsoleMaster", "VictorySeeker",
+    "GameJunkie", "PixelHunter", "Gametime", "Gamezilla", "MasterBlaster",
+    "GamingProdigy", "DungeonCrawler", "GameGladiator", "GamerDude", "ShadowGamer",
+    "GameOn", "VirtualWarrior", "QuestMaster", "GameGenius", "PlayerTwo",
+    "ArcadeKing", "LegendOfGames", "GameSlinger", "TacticalGamer", "GameBender",
+    "FantasyGamer", "GameHunter", "TheGamingBeast", "PowerPlayer", "GameFury",
+    "GameChampion", "PixelPirate", "GameWarlock", "UltimateGamer", "ProPlayer",
+    "GameSorcerer", "GamerGirl", "ControllerQueen", "BattleMistress", "PixelPrincess",
+    "VictoryVixen", "GameGoddess", "JoystickQueen", "ActionAngel", "GameEmpress",
+    "GamingQueen", "DungeonDiva", "GameMistress", "PixelHeroine", "QuestPrincess",
+    "GamePixie", "VirtualPrincess", "GamingVixen", "UltraGamerGirl", "GameWarriorWoman",
+    "GameLover", "ConsoleQueen", "VictoryVirtuosa", "GameDuchess", "GameExplorer",
+    "TheGamingLady", "PlayerThree", "ArcadeQueen", "GameSorceress", "GamerGal",
+    "TacticalGamerGirl", "GameBenderella", "FantasyGamerGirl", "GameHuntress", "TheGamingBeauty",
+    "PowerPlayerGirl", "GameFuryGirl", "GameChampioness", "PixelPirateLady", "GameWarlockess",
+    "UltimateGamerGirl", "ProGamerGirl", "GameSorceress", "GamerGoddess", "ControllerGoddess",
+    "BattleMistress", "PixelPrincess", "VictoryVixen", "GameGoddess", "JoystickQueen",
+    "ActionAngel", "GameEmpress", "GamingQueen", "DungeonDiva", "GameMistress",
+    "PixelHeroine", "QuestPrincess", "GamePixie", "VirtualPrincess", "GamingVixen",
+    "UltraGamerGirl", "GameWarriorWoman", "GameLover", "ConsoleQueen", "VictoryVirtuosa",
+    "GameDuchess", "GameExplorer", "TheGamingLady", "PlayerFour", "ArcadeQueen",
+    "GameSorceress", "GamerGal", "TacticalGamerGirl", "GameBenderella", "FantasyGamerGirl",
+    "GameHuntress", "TheGamingBeauty", "PowerPlayerGirl", "GameFuryGirl", "GameChampioness",
+    "PixelPirateLady", "GameWarlockess", "UltimateGamerGirl", "ProGamerGirl", "GameSorceress",
+    "GamerGoddess", "ControllerGoddess", "BattleMistress", "PixelPrincess", "VictoryVixen",
+    "GameGoddess", "JoystickQueen", "ActionAngel", "GameEmpress", "GamingQueen",
+    "DungeonDiva", "GameMistress", "PixelHeroine", "QuestPrincess", "GamePixie",
+    "VirtualPrincess", "GamingVixen", "UltraGamerGirl", "GameWarriorWoman", "GameLover",
+    "ConsoleQueen", "VictoryVirtuosa", "GameDuchess", "GameExplorer", "TheGamingLady",
+    "PlayerFive", "ArcadeQueen", "GameSorceress", "GamerGal", "TacticalGamerGirl",
+    "GameBenderella", "FantasyGamerGirl", "GameHuntress", "TheGamingBeauty", "PowerPlayerGirl"
+    };
     public Vector3 FirstTranfrom;
+    public string NameAI;
+    [SerializeField] Text NameAIText;
+
 
     private void Awake()
     {
-        botSprite = GetComponent<SpriteRenderer>();
-        botAnim = GetComponent<Animator>();
-        botAnim = GetComponent<Animator>();
+        anim = GetComponent<SkeletonAnimation>();
         agent = GetComponent<NavMeshAgent>();
-        FirstTranfrom = new Vector3( this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0);
+        FirstTranfrom = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0);
+        //NAME AI
+        int randomNum = Random.Range(0, aiNames.Length);
+        NameAI = aiNames[randomNum];
+        NameAIText.text = NameAI;
     }
     private void Start()
     {
@@ -35,8 +80,11 @@ public class MoveAI : MonoBehaviour
 
         if ((target.position.x - this.transform.position.x) < 0.1f && (target.position.y - this.transform.position.y) < 0.1f)
         {
+            //idle
             isRunning = false;
-            botAnim.SetBool("isRun", false);
+            anim.AnimationName = "stopandlose";
+            anim.Initialize(true);
+            //botAnim.SetBool("isRun", false);
             StartCoroutine(UpdateTask());
         }
         else
@@ -46,11 +94,15 @@ public class MoveAI : MonoBehaviour
             //flip
             if ((target.position.x - this.transform.position.x) < 0f)
             {
-                botSprite.flipX = true;
+                //botSprite.flipX = true;
+                anim.initialFlipX = false;
+                anim.Initialize(true);
             }
             else
             {
-                botSprite.flipX = false;
+                //botSprite.flipX = false;
+                anim.initialFlipX = true;
+                anim.Initialize(true);
             }
         }
 
@@ -74,7 +126,11 @@ public class MoveAI : MonoBehaviour
     private void ChangerTarget()
     {
         isRunning = true;
-        botAnim.SetBool("isRun", true);
+        anim.AnimationName = "run";
+        anim.loop = true;
+        anim.Initialize(true);
+
+        //botAnim.SetBool("isRun", true);
         int i = Random.Range(0, 10);
         target = GameManager.Instance.AllTranform[i];
     }
@@ -82,4 +138,19 @@ public class MoveAI : MonoBehaviour
 
 
 
+}
+public enum ColorBot
+{
+    black,
+    blue,
+    brown,
+    crystal,
+    green,
+    greenjungle,
+    orange,
+    pink,
+    purple,
+    red,
+    white,
+    yellow
 }
